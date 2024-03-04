7445df9a414361269728494a3c79dac7e2c2b9e3
Kuang-Pi <87300396+Kuang-Pi@users.noreply.github.com>
2024-03-03 22:49:08 -0500

    Changed

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,12 +6,18 @@ public class Bullet : MonoBehaviour
 {
     // Considering there might be effect that change the speed of the bullet after it is fired. 
     public float speed = 6f;
+    private Vector3 directionBullet = new();
     
+    private void Update()
+    {
+        this.transform.position += directionBullet * speed * Time.deltaTime;
+    }
+
     public void __init__(Vector3 position, Vector3 direction, float speed = 6f)
     {
         this.transform.position = position;
-        this.GetComponent<Rigidbody2D>().velocity = direction * speed;
+        directionBullet = direction.normalized;
         // Facing
-        this.transform.rotation = Quaternion.LookRotation(direction);
+        this.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(directionBullet.y, directionBullet.x) * Mathf.Rad2Deg);
     }
 }
